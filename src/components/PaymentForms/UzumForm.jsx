4416963f53d6b3df180be5e99c474a91663e91b1
MukhamedovK <kamoliddin2619@gmail.com>
2024-10-21 22:29:04 +0500

    fixed error

--- src/components/PaymentForms/UzumForm.jsx
+++ src/components/PaymentForms/UzumForm.jsx
@@ -5,8 +5,8 @@ const UzumForm = ({
   invoiceNumber,
   amount,
   clientName,
-  clientName,
   clientPhone,
+  clientAddress,
   passport,
   tgUsername,
   disabled,
@@ -16,18 +16,6 @@ const UzumForm = ({
       method="POST"
       action="https://www.apelsin.uz/open-service?serviceId=498614016"
     >
-      <input type="hidden" name="cash" value={`${amount}`} />
-
-      <input
-        type="hidden"
-        name="redirectUrl"
-        value="http://localhost:3000/course-info"
-      />
-
-      <input type="hidden" name="description" value={`${courseDescription}`} />
-
-      <input type="hidden" name="extraData" value={`${courseName}`} />
-
       <button
         className="flex items-center justify-center cursor-pointer space-x-2 p-4 w-48 rounded-lg shadow-md transition-all duration-300 transform hover:scale-105"
         disabled={disabled}
